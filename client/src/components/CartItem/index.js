4fb12831562fba7ca9fcd74b313a2cc74a4b7c75
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-07 18:45:11 -0700

    added add multiple to cart functionality and remove from cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = item => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+  };
+
+
   return (
     <div className="flex-row">
       <div>
@@ -21,8 +33,9 @@ const CartItem = ({ item }) => {
           <span
             role="img"
             aria-label="trash"
+            onClick={() => removeFromCart(item)}
           >
-            🗑️
+            🗑️        
           </span>
         </div>
       </div>
