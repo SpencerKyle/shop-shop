43700b4f995706fa2333c3efd1ca6111face3239
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-08 16:36:31 -0700

    finished shopping cart functionality

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -12,6 +12,23 @@ const CartItem = ({ item }) => {
     });
   };
 
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+    }
+  };
+
 
   return (
     <div className="flex-row">
@@ -29,13 +46,14 @@ const CartItem = ({ item }) => {
             type="number"
             placeholder="1"
             value={item.purchaseQuantity}
+            onChange={onChange}
           />
           <span
             role="img"
             aria-label="trash"
             onClick={() => removeFromCart(item)}
           >
-            🗑️        
+            🗑️
           </span>
         </div>
       </div>
