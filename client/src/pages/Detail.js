ba124f0bbfc3e02f41a92cbf34775e3b29b960ff
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-08 18:18:37 -0700

    added offline persisstence

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,6 +4,7 @@ import { useQuery } from '@apollo/client';
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS, REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART } from '../utils/actions';
 import Cart from "../components/Cart";
+import { idbPromise } from '../utils/helpers';
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
@@ -50,8 +51,21 @@ function Detail() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    //get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
